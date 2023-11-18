50af7cc7f111daec6de6375e3d0cef6567fc1ce4
Polikanov Pavel <polikanov.p@bk.ru>
2023-11-18 12:20:26 +0300

    'Create'

--- perevalapp/views.py
+++ perevalapp/views.py
@@ -7,3 +7,30 @@ from .serializers import *
 class PerevalViewSet(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        record = self.get_object()
+        if record.status == 'NW':
+            serializer = PerevalSerializer(record, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'state': '1',
+                        'message': 'Изменения внесены успешно'
+                    }
+                )
+            else:
+                return Response(
+                    {
+                        'state': '0',
+                        'message': serializer.errors
+                    }
+                )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': f'При данном статусе: {record.get_status_display()}, данные изменить нельзя!'
+                }
+            )
